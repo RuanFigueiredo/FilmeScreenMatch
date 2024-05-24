837ccbb109225adb90ed10da0b2024ccbe4c4187
RuanFigueiredo <ruanthiago67@gmail.com>
2024-05-24 20:37:03 -0300

    encapsulando código,adidionando métodos acessores e organizando em pacotes

--- src/Principal.java
+++ src/Principal.java
@@ -1,19 +1,22 @@
+import br.com.alura.screematch.modelos.Filme;
 
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
 
-        meuFilme.nome = "Vingadores - Era de Ultron";
-        meuFilme.anoDeLancamento = 2015;
-        meuFilme.duracaoEmMinutos = 130;
+        meuFilme.setNome( "Vingadores - Era de Ultron");
+        meuFilme.setAnoDeLancamento(2015);
+        meuFilme.setDuracaoEmMinutos(120);
 
         meuFilme.exibeFichaTecnica();
 
         meuFilme.avalia(8);
         meuFilme.avalia(10);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println(meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
+
+
+
 
     }
 }
---
+++ src/br/com/alura/screematch/modelos/Filme.java
@@ -0,0 +1,60 @@
+package br.com.alura.screematch.modelos;
+
+public class Filme {
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
+    public void exibeFichaTecnica(){
+        System.out.println("O nome do filme é: " + nome);
+        System.out.println("e  o ano de lançamento é: " + anoDeLancamento);
+        System.out.println("tem duração de : " + duracaoEmMinutos + "minutos");
+    }
+    public void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    public double pegaMedia(){
+        return somaDasAvaliacoes/totalDeAvaliacoes;
+    }
+}
