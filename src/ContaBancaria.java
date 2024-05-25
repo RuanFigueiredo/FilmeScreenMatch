a14cd4fbdbaabf717ba460e008860e0586681cc4
RuanFigueiredo <ruanthiago67@gmail.com>
2024-05-25 17:17:33 -0300

    exercícios não obrigatórios

---
+++ src/ContaBancaria.java
@@ -0,0 +1,21 @@
+public class ContaBancaria {
+    private int numConta;
+    private double saldo;
+    public String titular;
+
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
---
+++ src/Produto.java
@@ -0,0 +1,27 @@
+public class Produto {
+    private String nome;
+    private double preco;
+
+    private double precoNovo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void aplicarDesconto(double percentual){
+        double desconto = preco *(percentual/100);
+        preco-=desconto;
+    }
+}
