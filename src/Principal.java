3b89e7019feb2da645a01c7de8f6093adbe4a6c6
RuanFigueiredo <ruanthiago67@gmail.com>
2024-06-03 20:01:35 -0300

    consertando erro de sintaxe

--- src/Principal.java
+++ src/Principal.java
@@ -70,6 +70,6 @@ public class Principal {
         System.out.println("Tamanho da lista: " + listaDeFilmes.size());
         System.out.println("primeiro filme: " + listaDeFilmes.get(0).getNome());
 
-        System.out.println(listaDeFilmes.get);
+        System.out.println(listaDeFilmes);
     }
 }
